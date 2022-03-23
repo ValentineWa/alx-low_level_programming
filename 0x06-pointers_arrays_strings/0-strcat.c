2f190ff948dcf421900425769904f69c3f4a618d
ValentineWa <waweruval@gmail.com>
2022-03-23 03:00:44 -0700

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest, @src: pointer of type char
+ * Return: void
+ */
+char *_strcat (char *dest, char *src)
+{
+  int *a;
+  a = strcat (dest, src);
+  return (a);
+}
