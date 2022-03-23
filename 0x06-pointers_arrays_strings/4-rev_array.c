575980adb1ea71c5767482b0320494cc6cbffd72
ValentineWa <waweruval@gmail.com>
2022-03-23 08:46:06 -0700

    Compares two strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
