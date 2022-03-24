e57a5d3dff14e34564ace0425e69563a3378cbc5
ValentineWa <waweruval@gmail.com>
2022-03-24 13:13:45 -0700

    Changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,25 @@
 #include "main.h"
 /**
- * *_strncat - concatenates to strings 
+ * *_strncat - concatenates to strings
  * @dest, @src, @n type char
  * Return: pointer to resulting string
  */
 char *_strncat (char *dest, char *src, int n)
 {
-  
+  int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
